9cc9ddaeee3df05075708c70820838721d7633f6
Ketan Mistry <6877442+ketanmistry@users.noreply.github.com>
2024-12-06 06:48:38 +1000

    Fixed update qty so it ignores empty value.

--- assets/cart.js
+++ assets/cart.js
@@ -7,44 +7,46 @@ function updateLineItemQty(el) {
   let id = el.dataset.id;
   let quantity = el.value;
   let row = el.closest('.js--cart-item');
-  let data = {
-    id: key,
-    quantity: quantity
-  };
-  fetch('/cart/change.js', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(cart => {
-      console.log(cart);
-      if (quantity > 0) {
-        row.querySelectorAll('.js--cart-item-total').forEach(function(el) {
-          // Get details of the updated item.
-          let item = cart.items.filter(function(lineitem) {
-            if (lineitem.key === key) {
-              return lineitem;
-            }
+  if (quantity != "" && quantity >= 0) {
+    let data = {
+      id: key,
+      quantity: quantity
+    };
+    fetch('/cart/change.js', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(cart => {
+        console.log(cart);
+        if (quantity > 0) {
+          row.querySelectorAll('.js--cart-item-total').forEach(function(el) {
+            // Get details of the updated item.
+            let item = cart.items.filter(function(lineitem) {
+              if (lineitem.key === key) {
+                return lineitem;
+              }
+            });
+            el.innerText = moneyWithCurrency((item[0].final_line_price / 100));
           });
-          el.innerText = moneyWithCurrency((item[0].final_line_price / 100));
+        } else {
+          row.remove();
+          removeGiftWrap(key);
+        }
+        // Update the total cart price.
+        document.querySelectorAll('.js--cart-total').forEach(function(el) {
+          el.innerText = moneyWithCurrency(cart.total_price / 100);
         });
-      } else {
-        row.remove();
-        removeGiftWrap(key);
-      }
-      // Update the total cart price.
-      document.querySelectorAll('.js--cart-total').forEach(function(el) {
-        el.innerText = moneyWithCurrency(cart.total_price / 100);
+        if (typeof window.spendMore === "function") {
+          spendMore(cart);
+        }
+        // Update cart item count.
+        updateCartCounter(cart);
       });
-      if (typeof window.spendMore === "function") {
-        spendMore(cart);
-      }
-      // Update cart item count.
-      updateCartCounter(cart);
-    });
+  }
 };
 
 /*
