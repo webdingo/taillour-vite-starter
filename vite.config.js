eaddadd01534dfb2222721531c0b8229b0ec2921
webdingo <adrianweb92@gmail.com>
2025-05-26 20:35:54 +0100

    [global daisy ui and removed text section

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,14 @@ export default defineConfig({
 		}),
 		tailwindcss(),
 	],
+	server: {
+		cors: {
+			origin: [
+				/^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
+				"https://theme-development-boilerplates.myshopify.com",
+			],
+		},
+	},
 	css: {
 		postcss: "./postcss.config.js",
 	},
